e830532da08bbb965d58a91af60fff6e27f3637d
Jonas Ullvahn <ullvahn@gmail.com>
2024-11-24 18:01:33 +0100

    Add Sign in button for redirecting

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
 "use client";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-
 export default function Home() {
+  const router = useRouter();
 
+  const redirectLogin = () => {
+    router.push("/sign-in");
+  };
 
   return (
-    <main>
-      <div>
-       <h1>Home</h1>
-      </div>
-    </main>
+    <main className="flex flex-col items-center justify-center min-h-screen">
+    <h1 className="text-4xl font-bold mb-6">Welcome to the Home Page</h1>
+    <p className="text-lg text-gray-700 mb-4">Please log in to access your account details.</p>
+    <button
+      onClick={redirectLogin}
+      className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+    >
+      Go to Login
+    </button>
+  </main>
   );
 }
